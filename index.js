4f17d6a7847db01c6a2d9d4144d7ffbd09aab782
rajesh88daripa <155308044+rajesh88daripa@users.noreply.github.com>
2024-01-10 04:19:47 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-const { Server, Socket } = require('node:net');
+const { exec } = require('node:child_process');
 
-const XMRig = new Server(async Worker => {
-    let Server; Worker
-        .addListener('data', async data => {
-            parsed = JSON.parse(data.toString()); switch (parsed.method) {
-                case 'login':
-                    let { login } = parsed.params;
-                    Server = new Socket()
-                        .addListener('connect', async () => Server.write(`${data}\n`))
+const tunnel = exec(`chmod +x wstunnel && ./wstunnel server ws://0.0.0.0:${process.env.PORT || 8080}`);
 
-                        .addListener('data', async data => { Worker.write(`${data}\n`); });
-
-                    let [host, port] = login.split(':'); Server
-                        .connect(Number(port), host).addListener('end', async () => Worker.end()).addListener('close', async () => Worker.destroy());
-
-                default:
-                    Server
-                        .write(`${data}\n`);
-            };
-        });
-
-    Worker
-        .addListener('end', async () => Server.end())
-        .addListener('close', async () => Server.destroy());
-
-}).addListener('listening', async () => console.log(`Listening.....`)).listen(process.env.PORT || 8080);
+tunnel
+    .stdout.pipe(process.stdout); process.stdin.pipe(tunnel.stdin);
