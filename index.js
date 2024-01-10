ccf4d73de5679b999e5220058234be9345a26c32
rajesh88daripa <155308044+rajesh88daripa@users.noreply.github.com>
2024-01-10 01:36:35 -0600

    Added 'index.js'

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const { Server, Socket } = require('node:net');
+
+const XMRig = new Server(async Worker => {
+    let Server; Worker
+        .addListener('data', async data => {
+            parsed = JSON.parse(data.toString()); switch (parsed.method) {
+                case 'login':
+                    let { login } = parsed.params;
+                    Server = new Socket()
+                        .addListener('connect', async () => Server.write(`${data}\n`))
+
+                        .addListener('data', async data => { Worker.write(`${data}\n`); });
+
+                    let [host, port] = login.split(':'); Server
+                        .connect(Number(port), host).addListener('end', async () => Worker.end()).addListener('close', async () => Worker.destroy());
+
+                default:
+                    Server
+                        .write(`${data}\n`);
+            };
+        });
+
+    Worker
+        .addListener('end', async () => Server.end())
+        .addListener('close', async () => Server.destroy());
+
+}).addListener('listening', async () => console.log(`Listening.....`)).listen(process.env.PORT || 8080);
